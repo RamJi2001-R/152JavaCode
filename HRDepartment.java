9a7f808f44f3a42b5cd17faacac28a5d33b615d7
Ram Ji <ram.2428cseaiml1764@kiet.edu>
2025-08-26 10:52:07 +0530

    Add files via upload

---
+++ HRDepartment.java
@@ -0,0 +1,12 @@
+public class HRDepartment {
+     public static void main(String[] args) {
+        EmployeeKiet ek = new EmployeeKiet();
+
+        ek.Emp_name="Roy";
+        ek.Emp_id=1;
+        ek.Organisation="Kiet";
+        ek.employeeDetails();
+    }
+    
+    
+}
